d8ce03ad79ce9e3544ec0bfaff649d4fc5981371
Jakub Szewczyk <240810@edu.p.lodz.pl>
2023-03-07 15:58:35 +0100

    all printing and deleteing methods

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,23 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        ShoppingList shpopingList = new ShoppingList();
+        ShoppingList shoppingList = new ShoppingList();
         try{
-            shpopingList.loadList();
+            shoppingList.loadList();
         }
         catch (FileNotFoundException e){
             System.out.println("Plik nie istnieje!");
             System.exit(1);
         }
-        shpopingList.printShoppingList();
+        shoppingList.printShoppingList();
+        //shoppingList.printCategoryList("Motoryzacja");
+
+        shoppingList.deleteProductsFromCategory(2);
+        shoppingList.printShoppingList();
+        shoppingList.deleteSpecificProduct(1,1);
+        shoppingList.printShoppingList();
+
+
+
     }
 }
--- src/ShoppingList.java
+++ src/ShoppingList.java
@@ -4,17 +4,17 @@ import java.util.*;
 
 
 public class ShoppingList {
-    HashMap<String, List<String>> shoppingList;
+    LinkedHashMap<String, List<String>> shoppingList;
 
     public ShoppingList(){
-        shoppingList = new HashMap<String, List<String>>();
+        shoppingList = new LinkedHashMap<>();
     }
 
     public void loadList() throws FileNotFoundException {
         Scanner scanner = new Scanner(new FileReader("lista.txt"));
         String line;
         String category = null;
-        List<String> products = new ArrayList<String>();
+        List<String> products = new ArrayList<>();
         while(scanner.hasNextLine()){
             line = scanner.nextLine();
             if(line.charAt(0) != ' '){
@@ -22,7 +22,7 @@ public class ShoppingList {
                     shoppingList.put(category,products);
                 }
                 category = line;
-                products = new ArrayList<String>();
+                products = new ArrayList<>();
             }
             else{
                 products.add(line.trim());
@@ -38,8 +38,61 @@ public class ShoppingList {
             System.out.println("Kategoria: " + entry.getKey());
             List<String> products = entry.getValue();
             for (String product : products){
-                System.out.println("- " + product);
+                System.out.println("    - " + product);
             }
+        }
+        System.out.print("\n");
+    }
+
+    public void printCategoryList(String category){
+        for (Map.Entry<String, List<String>> entry : shoppingList.entrySet() ){
+            if(entry.getKey().equals(category)) {
+                List<String> products = entry.getValue();
+                int i = 1;
+                for (String product : products){
+                    System.out.println(i + ". " + product);
+                }
+                break;
+            }
+        }
+        System.out.print("\n");
+    }
+
+    public void printAllCategories(){
+        int i = 1;
+        System.out.println("Dostepne kategorie:");
+        for (Map.Entry<String, List<String>> entry : shoppingList.entrySet() ){
+            System.out.println(i+". " + entry.getKey());
+        }
+        System.out.print("\n");
+    }
+
+    public void deleteAllProducts(){
+        for (Map.Entry<String, List<String>> entry : shoppingList.entrySet() ){
+            entry.getValue().clear();
+        }
+        shoppingList.clear();
+    }
+
+    public void deleteProductsFromCategory(int category){
+        int i = 0;
+        for (Map.Entry<String, List<String>> entry : shoppingList.entrySet() ){
+            if(i == category){
+                entry.getValue().clear();
+                break;
+            }
+            ++i;
+        }
+    }
+
+    public void deleteSpecificProduct(int category, int product){
+        int i = 0;
+        for (Map.Entry<String, List<String>> entry : shoppingList.entrySet() ){
+            if(i == category){
+                 entry.getValue().remove(product);
+                 break;
+            }
+            ++i;
         }
     }
 
